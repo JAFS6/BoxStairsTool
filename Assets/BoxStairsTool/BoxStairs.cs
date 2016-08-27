5bcabc78689249634374ee13b01cafcad56f5745
Juan Antonio Fajardo Serrano <juan61088@gmail.com>
2016-08-27 21:24:16 +0200

    BoxStairs tool first version ready

--- Assets/BoxStairsTool/BoxStairs.cs
+++ Assets/BoxStairsTool/BoxStairs.cs
@@ -1,10 +1,81 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 namespace BoxStairsTool
 {
     [ExecuteInEditMode]
-    public class BoxStairs : MonoBehaviour
+    public sealed class BoxStairs : MonoBehaviour
     {
+        [SerializeField]
+        private float StairsWidth;
+        [SerializeField]
+        private float StairsHeight;
+        [SerializeField]
+        private float StairsDepth;
+        [SerializeField]
+        private int StepsNumber;
+        [SerializeField]
+        private Material StairsMaterial;
 
+        private GameObject Root;
+
+        private void Start()
+        {
+            Root = this.gameObject;
+
+            this.CreateStairs();
+        }
+
+        public BoxStairs()
+        {
+            StairsWidth = 1.0f;
+            StairsHeight = 0.5f;
+            StairsDepth = 1.0f;
+            StepsNumber = 2;
+            StairsMaterial = null;
+        }
+
+        public void CreateStairs()
+        {
+            // If any child has been created, destroy it
+
+            List<GameObject> children = new List<GameObject>();
+            children.Clear();
+
+            foreach (Transform child in Root.transform)
+            {
+                children.Add(child.gameObject);
+            }
+
+            while (children.Count > 0)
+            {
+                DestroyImmediate(children[0]);
+                children.RemoveAt(0);
+            }
+
+            // Create the new childs
+
+            float stepHeight = StairsHeight / StepsNumber;
+            float stepDepth = StairsDepth / StepsNumber;
+
+            for (int i = 0; i < StepsNumber; i++)
+            {
+                GameObject Step = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                Step.name = "Step " + i;
+                Step.transform.SetParent(Root.transform);
+                Step.transform.localScale = new Vector3(StairsWidth, stepHeight, StairsDepth - (i * stepDepth));
+                Step.transform.position = new Vector3(0, i * stepHeight, i * (stepDepth / 2));
+
+                if (StairsMaterial != null)
+                {
+                    Renderer renderer = Step.GetComponent<Renderer>();
+
+                    if (renderer != null)
+                    {
+                        renderer.material = StairsMaterial;
+                    }
+                }
+            }
+        }
     }
 }
--- Assets/BoxStairsTool/Editor/BoxStairsEditor.cs
+++ Assets/BoxStairsTool/Editor/BoxStairsEditor.cs
@@ -3,8 +3,50 @@ using UnityEditor;
 
 namespace BoxStairsTool
 {
-    public class BoxStairsEditor : Editor
+    [CustomEditor(typeof(BoxStairs))]
+    public sealed class BoxStairsEditor : Editor
     {
+        SerializedProperty StairsWidth;
+        SerializedProperty StairsHeight;
+        SerializedProperty StairsDepth;
+        SerializedProperty StepsNumber;
+        SerializedProperty StairsMaterial;
 
+        [MenuItem("BoxStairs Tool/Create BoxStairs")]
+        private static void CreateBoxStairsGO()
+        {
+            GameObject BoxStairs = new GameObject("BoxStairs");
+            BoxStairs.AddComponent<BoxStairs>();
+            Selection.activeGameObject = BoxStairs;
+        }
+
+        private void OnEnable()
+        {
+            StairsWidth = serializedObject.FindProperty("StairsWidth");
+            StairsHeight = serializedObject.FindProperty("StairsHeight");
+            StairsDepth = serializedObject.FindProperty("StairsDepth");
+            StepsNumber = serializedObject.FindProperty("StepsNumber");
+            StairsMaterial = serializedObject.FindProperty("StairsMaterial");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            EditorGUI.BeginChangeCheck();
+            EditorGUILayout.PropertyField(StairsWidth);
+            EditorGUILayout.PropertyField(StairsHeight);
+            EditorGUILayout.PropertyField(StairsDepth);
+            EditorGUILayout.PropertyField(StepsNumber);
+            EditorGUILayout.PropertyField(StairsMaterial);
+            serializedObject.ApplyModifiedProperties();
+            
+            if (EditorGUI.EndChangeCheck())
+            {
+                BoxStairs script = (BoxStairs)target;
+
+                script.CreateStairs();
+            }
+        }
     }
 }
